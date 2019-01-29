9ad60d82afb54f07efffd0977449ce773390e4f6
parsa mihandoost <mihandoost.p@gmail.com>
2019-01-29 22:34:02 +0330

    add more sample

--- src/main/java/ir/codefather/my_webb_app/ParsaController.java
+++ src/main/java/ir/codefather/my_webb_app/ParsaController.java
@@ -3,6 +3,7 @@ package ir.codefather.my_webb_app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,5 +15,16 @@ public class ParsaController {
         String name = request.getParameter("name");
         request.setAttribute("name", name);
         return "welcome";
+    }
+
+    /**
+     * this method do above method job in spring style
+     */
+    @RequestMapping("/spring-welcome")
+    public ModelAndView springWelcome(String name) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", name);
+        mv.setViewName("welcome");
+        return mv;
     }
 }
